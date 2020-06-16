6f1b6bc223a512443e9fb7cfee3be36aa222e260
Jane <yudina.e.a.23@mail.ru>
2020-06-16 13:51:05 +0300

    Модель и миграция таблицы с продукцией

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Product
+ *
+ * @property int $id
+ * @property int $article
+ * @property string $image_url
+ * @property string $name
+ * @property string $description
+ * @property string $availability
+ * @property string $category_id
+ * @property string $type_id
+ * @property int $published
+ * @property string $updated_ts
+ * @property string $created_ts
+ *
+ * @package App\Models
+ */
+
+class Product extends Model
+{
+    protected $table = 'products';
+    protected $guarded = ['id'];
+}
---
+++ database/migrations/2020_06_14_204606_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('article');
+            $table->string('image_url');
+            $table->string('name');
+            $table->string('description');
+            $table->string('availability');
+            $table->string('category_id');
+            $table->string('type_id');
+            $table->integer('published')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
