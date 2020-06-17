656e255377046c528c51b4111174ddc55be2e762
Jane <yudina.e.a.23@mail.ru>
2020-06-18 00:24:43 +0300

    Прописана поисковая строка по названию, корректировка вкладки каталог

--- app/Http/Controllers/Admin/CatalogController.php
+++ app/Http/Controllers/Admin/CatalogController.php
@@ -1,15 +1,37 @@
 <?php
 
-
 namespace App\Http\Controllers\Admin;
 
-
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class CatalogController extends Controller
 {
-    public function index() {
-        return \View::make('admin.catalog.catalog');
+
+    public function index (Request $request) {
+        $order = $request->get('order', 'id');
+        $order_type = $request->get('type', 'desc');
+        if (!in_array($order, ['id'])) {
+            $order = 'id';
+        }
+        if (!in_array($order_type, ['asc', 'desc'])) {
+            $order_type = 'desc';
+        }
+        $query = Product::orderBy($order, $order_type);
+
+        if ($request->exists('search')) {
+            $search = $request->get('search');
+            $search = '%' . $search . '%';
+            $query->where('name', 'LIKE', $search);
+        }
+
+        $items = $query->paginate(10);
+        return \View::make('admin.catalog.catalog', [
+            'items' => $items,
+            'search' => $request->get('search')
+        ]);
     }
 
     public function new () {
--- resources/views/admin/catalog/catalog.blade.php
+++ resources/views/admin/catalog/catalog.blade.php
@@ -6,22 +6,38 @@
         <h1 class="h2">Каталог продукции</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group mr-2">
-                <a type="button" href="" class="btn btn-sm btn-outline-secondary">+ Создать</a>
+                <a type="button" href="" class="btn btn-outline-success">+ Создать</a>
             </div>
         </div>
     </div>
+
+    <form action="{{ route('catalog_list') }}" method="get">
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <input type="text" class="form-control" name="search" placeholder="Поиск по названию">
+            </div>
+            <div class="form-group col-md-6">
+                <input type="submit" class="btn btn-outline-primary" value="Искать">
+                @if ($search != '')
+                    <a type="button" href="{{ route('catalog_list') }}" class="btn btn-outline-primary">Очистить поиск</a>
+                @endif
+            </div>
+        </div>
+    </form>
 
     <div class="table-responsive">
         <table class="table table-bordered table-hover">
             <thead>
             <tr class="table-warning">
                 <th scope="col"></th>
+                <th scope="col">
+                    <a href="{{ route('catalog_list', ['order' => 'id', 'type' => (isset($_GET['type']) && $_GET['type'] == 'asc') ? 'desc' : 'asc' ]) }}">id</a>
+                </th>
                 <th scope="col" style="width: 80px; text-align: center;">Артикул</th>
                 <th scope="col" style="width: 200px; text-align: center;">Фото</th>
                 <th scope="col" style="width: 200px; text-align: center;">Название</th>
                 <th scope="col" style="width: 250px; text-align: center;">Описание изделия</th>
                 <th scope="col" style="width: 80px; text-align: center;">Наличие</th>
-                <th scope="col" style="width: 80px; text-align: center;">Кол-во</th>
                 <th scope="col" style="text-align: center;">Категория</th>
                 <th scope="col" style="text-align: center;">Вид изделия</th>
                 <th scope="col" style="text-align: center;">Опубликовано</th>
@@ -30,60 +46,67 @@
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <td>
-                    <a type="button" href="catalog/view" class="btn btn-outline btn-primary btn-sm">Просмотр</a>
-                </td>
-                <th scope="row" style="max-width: 80px; overflow: hidden; text-align: center;">12345678</th>
-                <td style="max-width: 200px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">кlllllll88888888888888888888888llllllllllllllf</td>
-                <td style="max-width: 200px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">Названiiiiiiiooooooooooooooooooooooooooooooooooooooooooooooooiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiиеееееееееееееееее</td>
-                <td style="max-width: 250px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">Описание изделияeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</td>
-                <td style="max-width: 80px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; text-align: center;">Нет</td>
-                <td scope="row" style="max-width: 80px; text-align: center;">1234567</td>
-                <td>
-                    <div class="dropright">
-                        <button class="btn btn-warning btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown button
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Украшения ЭС</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Аксессуары для волос</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
+
+            @foreach($items as $item)
+                <?php /**  @var $item \App\Models\Product */ ?>
+                <tr>
+                    <td>
+                        <a type="button" href="" class="btn btn-outline-primary btn-sm">Просмотр</a>
+                    </td>
+                    <td>{{ $item->id }}</td>
+                    <td scope="row" style="max-width: 80px; overflow: hidden; text-align: center;">{{ $item->article }}</td>
+                    <td style="max-width: 200px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ $item->image_url }}</td>
+                    <td style="max-width: 200px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ $item->name }}</td>
+                    <td style="max-width: 250px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ $item->description }}</td>
+                    <td style="max-width: 80px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; text-align: center;">{{ $item->availability }}</td>
+                    <td>
+                        <div class="dropright">
+                            <button class="btn btn-outline-warning btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Категории
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Украшения ЭС</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Аксессуары для волос</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
                         </div>
-                    </div>
-                </td>
-                <td>
-                    <div class="dropright">
-                        <button class="btn btn-info btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown button
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Кулоны</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Заколки</a>
-                            <a class="dropdown-item" href="#">Маленькие игрушки</a>
+                    </td>
+                    <td>
+                        <div class="dropright">
+                            <button class="btn btn-outline-info btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Виды изделий
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Кулоны</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Заколки</a>
+                                <a class="dropdown-item" href="#">Маленькие игрушки</a>
+                            </div>
                         </div>
-                    </div>
-                </td>
-                <td>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
-                        <label class="form-check-label" for="exampleRadios1">Да</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" checked>
-                        <label class="form-check-label" for="exampleRadios2">Нет</label>
-                    </div>
-                </td>
-                <td>
-                    <a type="button" href="" class="btn btn-outline btn-success btn-sm">Редактировать</a>
-                </td>
-                <td>
-                    <a type="button" onclick="return confirm('Вы уверены?!')" href="" class="btn btn-outline btn-danger btn-sm">Удалить</a>
-                </td>
-            </tr>
+                    </td>
+                    <td>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
+                            <label class="form-check-label" for="exampleRadios1">Да</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" checked>
+                            <label class="form-check-label" for="exampleRadios2">Нет</label>
+                        </div>
+                    </td>
+                    <td>
+                        <a type="button" href="" class="btn btn-outline btn-success btn-sm">Редактировать</a>
+                    </td>
+                    <td>
+                        <a type="button" onclick="return confirm('Вы уверены?!')" href="" class="btn btn-outline btn-danger btn-sm">Удалить</a>
+                    </td>
+                </tr>
+
+            @endforeach
+
             </tbody>
         </table>
+        {{ $items->appends(Request::only(['search', 'order', 'type']))->links() }}
     </div>
 @endsection
