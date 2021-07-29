d80edf38fe8483247ef6b9fa69199d0a6dacec91
Jane <yudina.e.a.23@mail.ru>
2021-07-29 22:59:46 +0300

    Главная страница, рандомное выведение изделий в слайдере

--- app/Http/Controllers/LayoutController.php
+++ app/Http/Controllers/LayoutController.php
@@ -4,10 +4,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
 
 class LayoutController extends Controller
 {
     public function index() {
-        return \View::make('pages.index');
+        $products = Product::query()->get();
+        $categories = Category::query()->get();
+        return \View::make('pages.index', [
+            'products' => $products,
+            'categories' => $categories,
+        ]);
     }
 }
